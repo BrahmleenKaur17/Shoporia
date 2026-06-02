f29ea41b2e2682ba4ca695aed71a9b81eb653e2b
BrahmleenKaur17 <brahmleenkaur17@gmail.com>
2026-06-03 00:47:50 +0530

    Update server URL to production endpoint

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext } from "react";
 export const authDataContext = createContext();
 
 function AuthContext({ children }) {
-  let serverUrl = "http://localhost:8000";
+  let serverUrl = "https://shoporia-backend-9su9.onrender.com";
   let value = { serverUrl };
   return (
     <authDataContext.Provider value={value}>
